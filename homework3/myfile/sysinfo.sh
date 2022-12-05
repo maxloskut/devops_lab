ba38e9a6673caa41f3ec03577cd52795423dd073
max_loskut <artem.melent@gmail.com>
2022-12-05 09:08:05 +0200

    fix 3 and 4

--- homework3/myfile/sysinfo.sh
+++ homework3/myfile/sysinfo.sh
@@ -10,4 +10,4 @@ ss -tulpn >> /var/log/sysinfo.log
 
 ping -c2 ukr.net >> /var/log/sysinfo.log
 
-find / \( -perm -4000 -o -perm -2000 \) -type f -exec ls -la {} \; >> /var/log/sysinfo.log
+find / -perm -4000 >> /var/log/sysinfo.log
